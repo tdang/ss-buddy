94d001ae5bf726152d672602489f3fcab19b14d3
tdang <toan.s.dang@gmail.com>
2012-11-10 13:55:38 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SsBuddy::Application.config.secret_token = 'b83c2bcfc7e9a3b882aa9a1d7c6ea0ebdc8e068f231e27ed71edf285ef43775c0808684cfa67f395097efc8e0428020145f4d29bee5a270e171c5a9dc9422a3c'
